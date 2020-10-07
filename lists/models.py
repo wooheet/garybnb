aeb5e0988869af571a438453b6da25c4a1f911ad
gary <gary@spoonradio.co>
2020-10-07 18:25:37 +0900

    #8.2 Conversations, Lists, Reservations Admin

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from core import models as core_models
 
@@ -14,3 +15,8 @@ class List(core_models.TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def count_rooms(self):
+        return self.rooms.count()
+
+    count_rooms.short_description = "Number of Rooms"
